8ad6aa6264bb33b7a85a97f297627495b7d0123d
Sundy <1159279222@qq.com>
2017-06-08 21:33:20 +0800

    init chapter05

---
+++ chapter05/src/main/java/com/weituitu/chapter05/TestByteBufExamples.java
@@ -0,0 +1,11 @@
+package com.weituitu.chapter05;
+
+/**
+ * Created by Sundy on 2017/6/8.
+ */
+public class TestByteBufExamples {
+    public static void main(String[] args) {
+       // ByteBufExamples.heapBuffer();
+        ByteBufExamples.directBuffer();
+    }
+}
